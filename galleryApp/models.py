133651eb79facccdb7db76fb54b156999b6f3dd6
thatiah <thatiahnia@gmail.com>
2026-07-13 08:50:01 +0300

    make profile class in models.py

--- galleryApp/models.py
+++ galleryApp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, max_length=500)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+ 
+    def __str__(self):
+        return self.user.username
 
 
--- galleryApp/views.py
+++ galleryApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .models import Photo, Tag, PhotoInteraction, Profile
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -16,3 +19,15 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated.")
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user.profile)
+    return render(request, 'registration/profile.html', {'form': form})
