cee64e6913b6d0d5c93e33af82f813bd1b8f29f4
thatiah <thatiahnia@gmail.com>
2026-07-13 08:58:49 +0300

    add tag model

--- galleryApp/models.py
+++ galleryApp/models.py
@@ -9,5 +9,11 @@ class Profile(models.Model):
  
     def __str__(self):
         return self.user.username
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+ 
+    def __str__(self):
+        return self.name
 
 
