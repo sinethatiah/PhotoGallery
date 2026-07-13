aee9adc4ec6bd313b7544740d15e06b84f9a5a1c
thatiah <thatiahnia@gmail.com>
2026-07-13 10:26:40 +0300

    add form for updating profile

--- galleryApp/forms.py
+++ galleryApp/forms.py
@@ -2,15 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        
-    def clean_email(self):
+
+    def unique_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("An account with this email already exists.")
         return email
+    
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_picture']
