e5fbfa6e5f3bac37cdd93183452c7697e9e12dd5
thatiah <thatiahnia@gmail.com>
2026-07-15 11:30:02 +0300

    add url for password change

--- galleryApp/urls.py
+++ galleryApp/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
-    
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name='registration/password_change.html',
+        success_url='/profile/'
+    ), name='password_change'),
     path('', views.home, name='home'),
-    
+    path('photo/<int:pk>/', views.photo_detail, name='photo_detail'),
+    path('photo/<int:pk>/<str:interaction_type>/', views.toggle_interaction, name='toggle_interaction'),
 ]
